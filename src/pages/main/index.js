48f5885bc168e02dd5d430b4783ce28731079ddc
denismend <denisladeira1@gmail.com>
2020-07-26 00:32:16 -0300

    fix

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,16 +7,20 @@ import PeriodSelector from './SelectPeriod';
 import AppProvider from '../../hooks';
 
 import {
-  MainPage
+  MainPage,
+  PlansViewContainer
 } from './styles';
 
 const Main = () => {
   return (
     <AppProvider>
       <MainPage>
-        <HostGatorHeader />
-        <Header />
-        <PeriodSelector />
+        <HostGatorHeader /> { /* HostGator Header with logo */ }
+        <Header /> { /* App Header with absolute images */ }
+
+        <PlansViewContainer>
+          <PeriodSelector />
+        </PlansViewContainer>
 
       </MainPage>
     </AppProvider>
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
+import * as colors from '../../assets/colors';
+
 export const MainPage = styled.div``;
+
+export const PlansViewContainer = styled.div`
+  background-color: ${colors.lightGray};
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
